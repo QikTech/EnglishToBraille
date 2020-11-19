32c895c3ac3b5b0527d12367c8d466e280750057
Prasad sawant <prassaw@gmail.com>
2020-11-19 09:21:19 +0530

    Flask - Image to Text ready

--- imagetotext.py
+++
@@ -1,7 +0,0 @@
-import pytesseract as tess
-tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCRtesseract.exe'
-from PIL import Image
-
-img = Image.open('text.png')
-text = tess.image_to_string(img)
-print(text)
