2e13679ed968022cfef81c8fc9f37721eab04c79
Prasad sawant <prassaw@gmail.com>
2020-12-10 13:27:11 +0530

    Server Running

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Imported Decparations 
 // console.log("heya");
 const express = require("express");
 const app = express();
@@ -6,18 +7,27 @@ const multer = require("multer");
 const {TesseractWorker} = require('tesseract.js');
 const worker = new TesseractWorker();
 
+
+// CREATED STORAGE
 const storage = multer.diskStorage({
      destination: (req,res,cb) => {
-          cb(null,"./uploads")
+          cb(null, "./uploads");
      },
      filename: (req,res,cb)=>{
           cb(null, req.file);
      }
 });
 
-const upload = multer ({storage: storage}).single("tester");
 
+// SPECCIFIED UPLOADS
+const upload = multer ({storage: storage}).single("avatar");
 app.set("view engine", "ejs");
+
+
+// ROUTES
+app.get('/', (req,res) => {
+     res.render('index');
+})
 
 // app.get('/uploads', (req, res)=>{
 //      console.log('hey')
