10d4d89b04ab51f5d174ee913c98c4866595638f
Prasad sawant <prassaw@gmail.com>
2020-12-11 21:30:49 +0530

    Back to flask - understood the meaning of Microframework

---
+++ etb/anotherone.py
@@ -0,0 +1,11 @@
+# from flask import Flask, render_template
+
+# a=69
+
+# app = Flask(__name__)
+# @app.route('/')
+# def main():
+#      return render_template('hope.html', hop=a)
+
+#      if __name__ == "__name__":
+#           app.run()
