d860434551e6e340131553504f380fb95220ea88
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-03-25 22:34:27 +0530

    Image Display & Edited Text Working | Textbox Refresh Remaning

--- etb/app.py
+++ etb/app.py
@@ -17,11 +17,13 @@ app = Flask(__name__)
 
 # WHERE TO STORE UPLOADED IMAGE
 # Dom
-path = app.config['IMAGE_UPLOADS'] = "C:/Programming 0.1/Mlai/EnglishToBraille/etb/static/image/uploads/"
+path = app.config['IMAGE_UPLOADS'] = "C:/programming0.2/Mlai/EnglishToBraille/etb/static/image/uploads/"
 # FILE VALIDATIONS 
 app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG","JPEG","JPG"]
 
+our_image = ""
 uploaded_image = None
+is_plaintext = False
 
 @app.route('/')
 def main():
@@ -32,6 +34,8 @@ def main():
 def upload_image():
      if request.files:
           image = request.files["image"]
+          print(image)
+          
           print("image:  "+str(image))
           if image.filename == "":
                print("image must have a name")
@@ -47,13 +51,16 @@ def upload_image():
                lstr_img_name = str(image.filename).replace(" ", "_")
                image.save(os.path.join(app.config['IMAGE_UPLOADS'], lstr_img_name))
                # image.save(os.path.join(app.config['IMAGE_UPLOADS'], image.filename))
+          print(type(filename))
           print ('image saved ' + filename)
           global uploaded_image
           uploaded_image = filename
+          print(uploaded_image)
+          global our_image
+          our_image = "/static/image/uploads/" + uploaded_image
           return redirect(request.url)          
 
-
-     return render_template('app.html')
+     return render_template('app.html', uploaded = our_image)
 
 def allowed_image(filename):
      if not "." in filename:
@@ -79,18 +86,32 @@ def scan():
      print (plain_text)
      # Package Working
      a= first.aaa
+
+     # DISPLAY IMAGE
+     global our_image
+     # our_image = "/static/image/uploads/" + uploaded_image
      
      braille = alphaToBraille.translate(plain_text)
      print("text_scanned:\n"+plain_text)
      # print("text_translated:\n"+braille)
      
      # For braille Printing
-     return render_template('app.html',pt=plain_text)
+     return render_template('app.html',pt=plain_text,uploaded=our_image)
      
 
-@app.route('/send')
+@app.route('/send',methods=['GET','POST'])
 # def send():
 def show():
+     #SamBam33 was here
+     global is_plaintext
+     is_plaintext = True
+     if request.method == "POST":
+          is_plaintext = False
+          editedtext = request.form['changedtext']
+          print("-------------------------------------------")
+          print(editedtext)
+     #SamBam33 left here
+
      a=200
      print("path"+str(path))
      print("uploaded_image"+str(uploaded_image))
@@ -102,12 +123,12 @@ def show():
      # Package Working
      a= first.aaa
      
-     braille = alphaToBraille.translate(plain_text)
-     print("text_scanned:\n"+plain_text)
-     print("text_translated:\n"+braille)
+     braille = alphaToBraille.translate(editedtext)
+     # print("text_scanned:\n"+editedtext)
+     # print("text_translated:\n"+braille)
      
      # For braille Printing
-     return render_template('app.html', hop=a, t=braille, pt=plain_text)
+     return render_template('app.html', hop=a, t=braille, pt=plain_text, et=editedtext, ispt= is_plaintext)
      # # For Plain Text Printing
      # return render_template('app.html', hop=a, t=plain_text)
      
